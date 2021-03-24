c606df60d08267be51ac1faae8f4057198fe858b
sanvivarshney <76217397+sanvivarshney@users.noreply.github.com>
2021-03-24 22:50:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+canvas=document.getElementById ("canvas");
+ctx=canvas.getContext("2d");
+color="purple";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas=document.getElementById ("canvas");
+ctx=canvas.getContext("2d");
+color="cyan";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(230,200,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas=document.getElementById ("canvas");
+ctx=canvas.getContext("2d");
+color="lime";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(270,200,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas=document.getElementById ("canvas");
+ctx=canvas.getContext("2d");
+color="green";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(310,200,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas=document.getElementById ("canvas");
+ctx=canvas.getContext("2d");
+color="yellow";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(350,200,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas=document.getElementById ("canvas");
+ctx=canvas.getContext("2d");
+color="orange";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(390,200,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas=document.getElementById ("canvas");
+ctx=canvas.getContext("2d");
+color="red";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(430,200,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas=document.getElementById ("canvas");
+ctx=canvas.getContext("2d");
+color="pink";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(470,200,40,0,2*Math.PI);
+ctx.stroke();
